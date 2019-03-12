c51c5ace9a01e676cca0cb8439b9e8e85d34c5d2
Viktorija <viktorija.cvetanovska@hotmail.com>
2019-03-12 14:01:48 +0100

    changes page-portfolio

--- page-portfolio.php
+++ page-portfolio.php
@@ -6,6 +6,23 @@ if(have_posts()) :
     while (have_posts()) : the_post(); ?>
 
     <article class="post page ">
+
+     <! -- column-container -->
+     <div class="column-container clearfix">
+
+     <!-- title-column -->
+     <div class="tittle-column">
+     <h2><?php the_tittle(); ?></h2>
+
+     </div><!-- /title-column -->
+
+     <!-- text-column -->
+     <div class="text-column">
+     <?php the_content(); ?>
+     </div><!-- /text-column -->
+
+     </div><!-- /column-container --> 
+
     
        <?php the_content(); ?>
     </article>
