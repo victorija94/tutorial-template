6f76195dd5de9a41e9dd2f7e548e512229727f02
Viktorija <viktorija.cvetanovska@hotmail.com>
2019-03-14 00:00:18 +0100

    changes in page

--- page.php
+++ page.php
@@ -7,18 +7,26 @@ if(have_posts()) :
 
     <article class="post page ">
 
+    <nav class="site-nav children-links clearfix">
+
     <span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()); ?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
 
-    <?php
+<ul>
 
-    $args = array(
+        <?php
+
+        $args = array(
         'child_of' => get_top_ancestor_id(),
         'title_li' => ''
-    );
+       );
 
-    ?>
+   
+      ?>
 
-    <?php wp_list_pages( $args ); ?>
+      <?php wp_list_pages( $args ); ?>
+    </ul>
+    </nav>
+
        <h2><?php the_title(); ?></h2>
        <?php the_content(); ?>
     </article>
