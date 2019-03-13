465644f5f5e542594fe2948e617d9f187a556999
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-03-13 23:15:01 +0100

    bug fix

--- page.php
+++ page.php
@@ -1,31 +1,31 @@
 <?php
-//
-//get_header();
-//
-//if(have_posts()) :
-//    while (have_posts()) : the_post(); ?>
-<!---->
-<!--    <article class="post page ">-->
-<!---->
-<!--    --><?php
-//
-//    $args = array(
-//        'child_of' => get_top_ancestor_id()
-//        'tittle_li' =>''
-//    );
-//
-//    ?>
-<!---->
-<!--    --><?php //wp_list_pages( $args ); ?>
-<!--       <h2>--><?php //the_title(); ?><!--</h2>-->
-<!--       --><?php //the_content(); ?>
-<!--    </article>-->
-<!--    --><?php //endwhile;
-//
-//    else:
-//        echo "";
-//    endif;
-//
-//
-//    get_footer();
-//?>
+
+get_header();
+
+if(have_posts()) :
+    while (have_posts()) : the_post(); ?>
+
+    <article class="post page ">
+
+    <?php
+
+    $args = array(
+        'child_of' => get_top_ancestor_id(),
+        'tittle_li' => ''
+    );
+
+    ?>
+
+    <?php wp_list_pages( $args ); ?>
+       <h2><?php the_title(); ?></h2>
+       <?php the_content(); ?>
+    </article>
+    <?php endwhile;
+
+    else:
+        echo "";
+    endif;
+
+
+    get_footer();
+?>
