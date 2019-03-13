50428fb57bb824411d7e3633afa7bfcc133082db
Viktorija <viktorija.cvetanovska@hotmail.com>
2019-03-13 01:41:39 +0100

    changes in special-template,changes style

--- special-template.php
+++ special-template.php
@@ -11,6 +11,14 @@ if(have_posts()) :
 
     <article class="post page ">
        <h2><?php the_title(); ?></h2>
+
+       <!-- info-box -->
+       <div class="info-box">
+         <h4>Disclaimer Title</h4>
+         <p>Skopje is Macedonia’s capital city and cultural hub – a stopover point that merits a minimum of a couple of days.</p>
+
+       </div><!-- /info-box -->
+
        <?php the_content(); ?>
     </article>
     <?php endwhile;
