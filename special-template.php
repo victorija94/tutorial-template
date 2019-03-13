3a6ef39b56a002630972e123610a3649c53cbc0b
Viktorija <viktorija.cvetanovska@hotmail.com>
2019-03-13 01:24:53 +0100

    added special-template

---
+++ special-template.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Template Name: Special Layout
+*/
+
+get_header();
+
+if(have_posts()) :
+    while (have_posts()) : the_post(); ?>
+
+    <article class="post page ">
+       <h2><?php the_title(); ?></h2>
+       <?php the_content(); ?>
+    </article>
+    <?php endwhile;
+
+    else:
+        echo "";
+    endif;
+
+
+    get_footer();
+?>
