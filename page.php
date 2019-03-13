4e50ef44f976e5d1e472ea47fb21f26916de78f1
Viktorija <viktorija.cvetanovska@hotmail.com>
2019-03-13 14:42:35 +0100

    changes page

--- page.php
+++ page.php
@@ -6,6 +6,16 @@ if(have_posts()) :
     while (have_posts()) : the_post(); ?>
 
     <article class="post page ">
+
+    <?php
+
+    $args = array(
+        'child_of' => $post->ID
+    );
+
+    ?>
+
+    <?php wp_list_pages( $args ); ?>
        <h2><?php the_title(); ?></h2>
        <?php the_content(); ?>
     </article>
