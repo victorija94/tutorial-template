92b5373ec44dac1abb2fbdf4945108fdcf4a7802
Viktorija <viktorija.cvetanovska@hotmail.com>
2019-03-19 00:24:27 +0100

    changes index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if(have_posts()) :
     <article class="post">
        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-<p class="post-info"><?php the_time('F jS, Y');?></p>
+<p class="post-info"><?php the_time('F jS, Y g:i a');?></p>
 
        <?php the_content(); ?>
     </article>
