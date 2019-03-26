0417c6db94e5bed3f0b60f1a85c6a25b020480e9
victorija94 <viktorija.cvetanovska@hotmaill.com>
2019-03-27 00:50:54 +0100

    changes functions

--- functions.php
+++ functions.php
@@ -2,11 +2,7 @@
 
 add_action('wp_enqueue_scripts', 'learningWordPress_resources');
 
-//Navigation Menus
-register_nav_menus(array(
-    'primary' => __('Primary Menu'),
-    'footer' => __('Footer Menu '),
-));
+
 
 function learningWordPress_resources(){
     wp_enqueue_style('style', get_stylesheet_uri());
@@ -42,8 +38,16 @@ function custom_excerpt_length() {
 add_filter('excerpt_length' , 'custom_excerpt_length');
 
 
-// Add featured image support
+// Theme setup
 function learningWordPress_setup() {
+
+    //Navigation Menus
+register_nav_menus(array(
+    'primary' => __('Primary Menu'),
+    'footer' => __('Footer Menu '),
+));
+
+    // Add featured image support
     add_theme_support('post_thumbnails');
 
 }
