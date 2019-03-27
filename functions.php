05497159d1139f82dc11384807885c4d5cf16228
victorija94 <viktorija.cvetanovska@hotmaill.com>
2019-03-27 01:24:56 +0100

    changes functions

--- functions.php
+++ functions.php
@@ -49,6 +49,8 @@ register_nav_menus(array(
 
     // Add featured image support
     add_theme_support('post_thumbnails');
+    add_image_size('small-thumbnail', 180, 120, true );
+    add_image_size('banner-image', 920, 210, true );
 
 }
 
