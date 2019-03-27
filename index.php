d47d48837c75176c2e4c7c40d712016ed0a00c8a
victorija94 <viktorija.cvetanovska@hotmaill.com>
2019-03-27 22:36:10 +0100

    changes style

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ get_header();
 if(have_posts()) :
     while (have_posts()) : the_post(); ?>
 
-    <article class="post has-thumbnail">
+    <article class="post <?php if ( has_post_thumbnail() ) { ?>has-thumbnail <?php } ?>">
 
     <!-- post-thumbnail --> 
     <div class="post-thumbnail">
