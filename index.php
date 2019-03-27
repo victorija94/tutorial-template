d2cfb039fb246dee6e4c736d4ab55fd16f5986ec
victorija94 <viktorija.cvetanovska@hotmaill.com>
2019-03-27 22:21:07 +0100

    changes index

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ if(have_posts()) :
     while (have_posts()) : the_post(); ?>
 
     <article class="post">
+
+    <!-- post-thumbnail --> 
+    <div class="post-thumbnail">
+       <?php the_post_thumbnail('small-thumbnail'); ?>
+
+    </div><!-- /post-thumbnail --> 
+   
+
+
        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
 <p class="post-info"><?php the_time('F jS, Y g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); 
@@ -34,7 +43,6 @@ echo trim($output, $separator);
 
  </p>
 
- <?php the_post_thumbnail('small-thumbnail'); ?>
 
  <?php if ($post->post_excerpt) { ?>
 
