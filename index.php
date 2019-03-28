3ae5428872878d40e5b8a7ebced53cbae9a44463
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-03-28 23:52:04 +0100

    bug fix in index

--- index.php
+++ index.php
@@ -5,126 +5,56 @@ get_header();
 if (have_posts()) :
     while (have_posts()) : the_post(); ?>
 
-    <article class="post <?php if (has_post_thumbnail()) { ?>has-thumbnail <?php } ?>">
+        <article class="post <?php if (has_post_thumbnail()) { ?>has-thumbnail <?php } ?>">
 
-        <!-- post-thumbnail -->
-        <div class="post-thumbnail">
-            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
-        </div><!-- /post-thumbnail -->
+            <!-- post-thumbnail -->
+            <div class="post-thumbnail">
+                <a href="<?php the_permalink(); ?>"<?php the_post_thumbnail('small-thumbnail'); ?></a>
+            </div><!-- /post-thumbnail -->
 
 
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-        <p class="post-info"><?php the_time('F jS, Y g:i a'); ?> | by <a
-                    href="<?php echo get_author_posts_url(get_the_author_meta('ID'));
-                    ?> "><?php the_author(); ?></a> | Posted in
+            <p class="post-info"><?php the_time('F jS, Y g:i a'); ?> | by <a
+                        href="<?php echo get_author_posts_url(get_the_author_meta('ID'));
+                        ?> "><?php the_author(); ?></a> | Posted in
 
-            <?php
+                <?php
 
-            $categories = get_the_category();
-            $separator = ", ";
-            $output = '';
+                $categories = get_the_category();
+                $separator = ", ";
+                $output = '';
 
-            if ($categories) {
+                if ($categories) {
 
-                foreach ($categories as $category) {
+                    foreach ($categories as $category) {
 
-                    $output .= '<a href="' . get_category_link($category->term_id) . '">' .
-                        $category->cat_name . '</a>' . $separator;
+                        $output .= '<a href="' . get_category_link($category->term_id) . '">' .
+                            $category->cat_name . '</a>' . $separator;
+
+                    }
+
+                    echo trim($output, $separator);
 
                 }
 
-                echo trim($output, $separator);
+                ?>
 
-            }
-
-            ?>
-
-        </p>
-
-
-        <?php if ($post->post_excerpt) { ?>
-
-            <p>
-            <?php echo get_the_excerpt(); ?>
-            <a <?php
-
-            get_header();
-
-            if (have_posts()) :
-                while (have_posts()) : the_post(); ?>
-
-                    <article class="post <?php if (has_post_thumbnail()) { ?>has-thumbnail <?php } ?>">
-
-                        <!-- post-thumbnail -->
-                        <div class="post-thumbnail">
-                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
-                        </div><!-- /post-thumbnail -->
-
-
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-                    <p class="post-info"><?php the_time('F jS, Y g:i a'); ?> | by <a
-                                href="<?php echo get_author_posts_url(get_the_author_meta('ID'));
-                                ?> "><?php the_author(); ?></a> | Posted in
-
-                        <?php
-
-                        $categories = get_the_category();
-                        $separator = ", ";
-                        $output = '';
-
-                        if ($categories) {
-
-                            foreach ($categories as $category) {
-
-                                $output .= '<a href="' . get_category_link($category->term_id) . '">' .
-                                    $category->cat_name . '</a>' . $separator;
-
-                            }
-
-                            echo trim($output, $separator);
-
-                        }
-
-                        ?>
-
-                    </p>
-
-
-                    <?php if ($post->post_excerpt) { ?>
-
-                        <p>
-                            <?php echo get_the_excerpt(); ?>
-                            <a href="<?php the_permalink(); ?>">Read more&raquo;</a>
-                        </p>
-
-                    <?php } else {
-
-                        the_content();
-
-                    } ?>
-
-
-                    </article>
-
-                <?php endwhile;
-
-            else:
-                echo "";
-            endif;
-
-
-            get_footer();
-            ?>
-            href="<?php the_permalink(); ?>">Read more&raquo;</a>
             </p>
 
-        <?php } else {
 
-            the_content();
+            <?php if ($post->post_excerpt) { ?>
 
-        } ?>
+                <p>
+                    <?php echo get_the_excerpt(); ?>
+                    <a href="<?php the_permalink(); ?>">Read more&raquo;</a>
+                </p>
+
+            <?php } else {
+
+                the_content();
+
+            } ?>
 
 
         </article>
