3885a0941ecbc3b90ade78ff504aec593fbcd8a8
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-01 00:50:04 +0200

    changes footer

--- footer.php
+++ footer.php
@@ -3,25 +3,25 @@
 <!-- footer-widgets --> 
 <div class="footer-widgets clearfix">
 
-<?php if (is_active_sidebar('footer1')); ?>
+<?php if (is_active_sidebar('footer1')): ?>
     <div class="footer-widget-area">
     <?php dynamic_sidebar('footer1'); ?>
     </div>
 <?php endif; ?>
 
-<?php if (is_active_sidebar('footer2')); ?>
+<?php if (is_active_sidebar('footer2')): ?>
     <div class="footer-widget-area">
     <?php dynamic_sidebar('footer2'); ?>
     </div>
 <?php endif; ?>
 
-<?php if (is_active_sidebar('footer3')); ?>
+<?php if (is_active_sidebar('footer3')): ?>
     <div class="footer-widget-area">
     <?php dynamic_sidebar('footer3'); ?>
     </div>
 <?php endif; ?>
 
-<?php if (is_active_sidebar('footer4')); ?>
+<?php if (is_active_sidebar('footer4')): ?>
     <div class="footer-widget-area">
     <?php dynamic_sidebar('footer4'); ?>
     </div>
