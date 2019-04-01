0579bcb2cb3f9d75f9c3c81c964639d6d1d69c11
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-01 23:45:05 +0200

    changes function

--- functions.php
+++ functions.php
@@ -70,9 +70,9 @@ function ourWidgetsInit() {
 
     register_sidebar( array(
         'name' => 'Footer Area 1',
-        'id' => 'footer1'
+        'id' => 'footer1',
         'before_widget' => '<div class="widget-item">',
-        'after_widget' => '</div>'
+        'after_widget' => '</div>',
         'before_title' => '<h4 class="my-special-class">',
         'after_title' => '</h4>'
         ));
