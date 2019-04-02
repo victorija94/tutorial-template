692836c9f98a2002c0dd7f1a2d73a413cc586ed7
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-03 00:15:26 +0200

    bug fix

--- front-page.php
+++ front-page.php
@@ -21,21 +21,10 @@
 
         if ($opinionPosts->have_posts()) :
 
-<<<<<<< HEAD
 // opinion posts loop begins here
 $opinionPosts = new WP_Query('cat=5');
 
 if ($opinionPosts->have_posts()) : 
-=======
-            while ($opinionPosts->have_posts()) :
-                $opinionPosts->the_post(); ?>
-                <h2><?php the_title(); ?></h2>
-            <?php endwhile;
-
-        else:
-            // fallback no content message here
-        endif;
->>>>>>> 874fb35f547d9a74342136ab0d1ed601e4b9bb48
 
         ?>
 
