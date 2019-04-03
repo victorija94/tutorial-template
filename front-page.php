92535c1a47af784564da471ab76ee314d826a89a
victorija94 <48369260+victorija94@users.noreply.github.com>
2019-04-03 17:52:00 +0200

    Bug fix

--- front-page.php
+++ front-page.php
@@ -7,7 +7,8 @@
         <?php if (have_posts()) :
             while (have_posts()) : the_post();
 
-                the_content();
+get_template_part('content', get_post_format());
+        
 
             endwhile;
         else:
