eebd8a1ae71ceebfabc48d61ce4e321a870415b4
victorija94 <48369260+victorija94@users.noreply.github.com>
2019-04-06 01:03:50 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,51 +1,80 @@
-<footer class="site-footer">
+<!-- Footer -->
+		<div id="featured">
+			<div class="container">
+				<div class="row">
+					<div class="4u">
+						<h2>Aenean elementum facilisis</h2>
+						<a href="#" class="image full"><img src="images/pic01.jpg" alt="" /></a>
+						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
+						<p><a href="#" class="button">More Details</a></p>
+					</div>
+					<div class="4u">
+						<h2>Fusce ultrices fringilla</h2>
+						<a href="#" class="image full"><img src="images/pic02.jpg" alt="" /></a>
+						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
+						<p><a href="#" class="button">More Details</a></p>
+					</div>
+					<div class="4u">
+						<h2>Etiam rhoncus volutpat erat</h2>
+						<a href="#" class="image full"><img src="images/pic03.jpg" alt="" /></a>
+						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
+						<p><a href="#" class="button">More Details</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
 
-<!-- footer-widgets --> 
-<div class="footer-widgets clearfix">
 
-<?php if (is_active_sidebar('footer1')): ?>
-    <div class="footer-widget-area">
-    <?php dynamic_sidebar('footer1'); ?>
-    </div>
-<?php endif; ?>
+		<!-- Footer -->
+		<div id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="4u">
+						<section>
+							<h2>Latest Posts</h2>
+							<ul class="default">
+								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
+								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
+								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
+								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
+								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
+							</ul>
+						</section>
+					</div>
+					<div class="4u">
+						<section>
+							<h2>Ultrices fringilla</h2>
+							<ul class="default">
+								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
+								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
+								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
+								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
+								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
+							</ul>
+						</section>
+					</div>
+					<div class="4u">
+						<section>
+							<h2>Aenean elementum</h2>
+							<ul class="default">
+								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
+								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
+								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
+								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
+								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
+							</ul>
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
 
-<?php if (is_active_sidebar('footer2')): ?>
-    <div class="footer-widget-area">
-    <?php dynamic_sidebar('footer2'); ?>
-    </div>
-<?php endif; ?>
-
-<?php if (is_active_sidebar('footer3')): ?>
-    <div class="footer-widget-area">
-    <?php dynamic_sidebar('footer3'); ?>
-    </div>
-<?php endif; ?>
-
-<?php if (is_active_sidebar('footer4')): ?>
-    <div class="footer-widget-area">
-    <?php dynamic_sidebar('footer4'); ?>
-    </div>
-<?php endif; ?>
-
-</div><!-- /footer-widgets --> 
-
-<nav class="site-nav">
-  <?php
-
-$args = array(
-  'theme_location' => 'footer');
-
-?>
-
-<?php wp_nav_menu($args); ?>
-</nav>
-
-<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y');?></p>
-
-</footer>
-
-</div><!-- container -->
-
-<?php wp_footer(); ?>
-</body>
+		<!-- Copyright -->
+		<div id="copyright">
+			<div class="container">
+				Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
+			</div>
+		</div>
+		
+	</body>
 </html>
