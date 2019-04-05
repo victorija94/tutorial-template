9bd03bbe52901ad9cbbd7462f0f9129698c99c65
victorija94 <48369260+victorija94@users.noreply.github.com>
2019-04-06 01:00:46 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,43 +1,55 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo('charset'); ?>">
+<!DOCTYPE HTML>
+<!--
+	Iridium by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Iridium by TEMPLATED</title>
+         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width">
         <title><?php bloginfo('name'); ?></title>
         <?php wp_head(); ?>
-</head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body class="homepage">
 
-<body <?php body_class(); ?>>
+		<!-- Header -->
+		<div id="header">
+			<div class="container"> 
+				
+				<!-- Logo -->
+				<div id="logo">
+					<h1><a href="#">Iridium</a></h1>
+					<span>Design by TEMPLATED</span>
+				</div>
+				
+				<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li class="active"><a href="index.html">Homepage</a></li>
+						<li><a href="left-sidebar.html">Left Sidebar</a></li>
+						<li><a href="right-sidebar.html">Right Sidebar</a></li>
+						<li><a href="no-sidebar.html">No Sidebar</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
 
-<div class="container">
-
-  <!--site-header-->
-  <header class="site-header">
-
-<!-- hd-search -->
-<div class="hd-search">
-    <?php get_search_form(); ?>
-</div><!-- /hd-search --> 
-
-  <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-  <h5><?php bloginfo('description'); ?> 
-  <?php if (is_page(2)) { ?>
-  - Thank you for viewing our work
-<?php }?></h5>
-
-  
-
-<nav class="site-nav">
-
-<?php
-
-$args = array(
-  'theme_location' => 'primary');
-
-?>
-
-<?php wp_nav_menu($args); ?>
-
-</nav>
-
-  </header><!-- /site-header --> 
+		<!-- Main -->
