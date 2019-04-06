a77076799d87511b8531145e45ceb4624b293ed9
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-06 23:53:50 +0200

    bug fix

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<?php
-
-if (have_posts()) :
-    while (have_posts()) : the post();
-
-    <h2><?php the_title()?></h2>
-<?php the_content() ?>
-
+<?php 
+    if (have_posts()) : 
+        while (have_posts()) : the post(); ?>
+            <h2><?php the_title()?></h2>
+            <?php the_content() ?>
 <?php endwhile;
-else :
-    echo '<p>No content found</p>';
-endif;
-
+    else :
+        echo '<p>No content found</p>';
+    endif;
 ?>
