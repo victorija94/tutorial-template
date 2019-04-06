cd6e2de2063882621f46235932f2c7a34d85a476
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-06 23:46:04 +0200

    changes index

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<H1>Testing</h1>
+<?php
+
+if (have_posts()) :
+    while (have_posts()) : the post();
+
+    <h2><?php the_title()?></h2>
+<?php the_content() ?>
+
+<?php endwhile;
+else :
+    echo '<p>Nocontent found</p>';
+endif;
+
+?>
