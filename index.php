654115c4fd5a2def3fe14eb0e2674d82013421d1
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-07 00:07:09 +0200

    changes index

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<?php 
+<?php
+
+get_header();
+
     if (have_posts()) : 
         while (have_posts()) : the_post(); ?>
-            <h2><?php the_title()?></h2>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <?php the_content() ?>
 <?php endwhile;
     else :
         echo '<p>No content found</p>';
     endif;
+
+    get_footer();
 ?>
