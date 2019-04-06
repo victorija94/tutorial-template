57f215e2b136f103991b51d71a9b6c1fa91d71d5
victorija94 <48369260+victorija94@users.noreply.github.com>
2019-04-06 02:07:06 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,12 +7,14 @@ function learningWordPress_resources(){
     wp_register_script( 'custom-script', get_template_directory_uri() . '/js/skel.min.js');
     wp_register_script( 'custom-script', get_template_directory_uri() . '/js/skel-panels.min.js');
     
-    wp_enqueue_style('custom-styles', get_stylesheet_uri());
-    wp_enqueue_style( 'custom-styles', get_template_directory_uri() . '/css/skel-noscript.css');
+    wp_register_style('custom-styles123', get_stylesheet_uri());
+
+    wp_enqueue_style( 'custom-styles123', get_template_directory_uri() . '/css/skel-noscript.css');
     wp_enqueue_style( 'custom-styles', get_template_directory_uri() . '/css/style.css');
-    wp_enqueue_style( 'custom-styles', get_template_directory_uri() . '/css/style-desktop.css');
-    wp_enqueue_style( 'custom-styles', get_template_directory_uri() . '/css/style-1000px.css');
-    wp_enqueue_style( 'custom-styles', get_template_directory_uri() . '/css/style-mobile.css');
+    wp_enqueue_style('custom-styles')
+    wp_enqueue_style( 'custom-styles123', get_template_directory_uri() . '/css/style-desktop.css');
+    wp_enqueue_style( 'custom-styles123', get_template_directory_uri() . '/css/style-1000px.css');
+    wp_enqueue_style( 'custom-styles123', get_template_directory_uri() . '/css/style-mobile.css');
 }
 
 add_action('wp_enqueue_scripts', 'learningWordPress_resources');
