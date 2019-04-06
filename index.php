5915989678534e2bf6ebf158e35358ae62642f20
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-07 00:24:46 +0200

    bug fix

--- index.php
+++ index.php
@@ -1,15 +1,93 @@
-<?php
-
-get_header();
-
-    if (have_posts()) : 
-        while (have_posts()) : the_post(); ?>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <?php the_content() ?>
-<?php endwhile;
-    else :
-        echo '<p>No content found</p>';
-    endif;
-
-    get_footer();
-?>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+<body>
+<div id="output_width">
+</div>
+<!--Widow width inserted here-->
+<div id="container">
+  <header class="main-header">
+    <img id="logo" src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.png" alt="Responsive Design" />
+    <nav class="main-nav">
+      <ul>
+        <li><a class="active" href="#">Home</a></li>
+        <li><a href="#">Learn</a></li>
+        <li><a href="#">Brainstorm</a></li>
+        <li><a href="#">Create</a></li>
+        <li><a href="#">Collaborate</a></li>
+        <li><a href="#">Teach</a></li>
+      </ul>
+    </nav>
+    <!--./main-nav-->
+  </header>
+  <!--./main-header-->
+  <div id="main">
+    <div id="content">
+      <article class="art1">
+        <header class="art-header">
+          <h2 class="title"><a href="#">First Post</a></h2>
+          <span>Created by: Brad Traversy on October 3, 2013</span>
+        </header>
+        <!--./art-header-->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus arcu est, lacinia et nisi nec, vehicula pretium magna. Nulla ut nisl vel lorem posuere aliquam a feugiat sapien. Suspendisse tincidunt mauris at consequat ornare. Donec condimentum
+          consequat felis a sodales. Donec vestibulum imperdiet nisl id vulputate. </p>
+        <p>Aenean in tellus ut nibh dignissim adipiscing nec eget libero. Donec eu diam quam. Sed fermentum elementum ultrices. Aliquam erat volutpat. Integer sollicitudin lacus quis est elementum tincidunt. Praesent mattis, felis a hendrerit tempus, risus
+          orci mollis massa, a porta lorem libero id purus. </p>
+        <a class="readmore" href="#">Read more</a>
+      </article>
+      <!--./art1-->
+      <article class="art1">
+        <header class="art-header">
+          <h2 class="title"><a href="#">Second Post</a></h2>
+          <span>Created by: Brad Traversy on October 3, 2013</span>
+        </header>
+        <!--./art-header-->
+        <p>In in rutrum sapien. Ut tellus nibh, facilisis faucibus pulvinar vitae, sagittis at orci. In sem tellus, faucibus non eros sit amet, tincidunt dictum ligula. Fusce ornare sodales nisi, non aliquet lacus rutrum in. Vestibulum in sem urna. Cras
+          commodo massa enim, non hendrerit lacus pretium vel. </p>
+        <p>Aliquam tristique augue vitae urna aliquet, et volutpat metus gravida. Mauris nec ornare nisl. Sed egestas mi et viverra sollicitudin. Fusce hendrerit adipiscing ipsum, ut suscipit nibh convallis fringilla. Mauris tempus id velit at lacinia. Aenean
+          scelerisque ac sem in accumsan. </p>
+        <a class="readmore" href="#">Read more</a>
+      </article>
+      <!--./art1-->
+    </div>
+    <!--./content-->
+    <aside id="sidebar">
+      <div class="block">
+        <h2>Top Sidebar</h2>
+        <p>
+          Donec eu diam quam sed fermentum elementum ultrices aliquam erat volutpat. In gravida nunc a metus sodales egestas. Morbi aliquet metus ultricies cursus elementum.
+        </p>
+      </div>
+      <!--./side1-->
+      <div class="block">
+        <h2>Mid Sidebar</h2>
+        <p>
+          Vivamus arcu est quam sed fermentum elementum ultrices aliquam erat volutpat. Vestibulum dolor dolor, tincidunt vitae augue semper, semper gravida neque pellentesque eleifend.
+        </p>
+      </div>
+      <!--./side2-->
+      <div class="block">
+        <h2>Bottom Sidebar</h2>
+        <p>
+          Lorem ipsum dolor sit amet quam sed fermentum elementum ultrices aliquam erat volutpat. Proin porta ornare libero, eu aliquet tortor vestibulum sed.
+        </p>
+      </div>
+         <div class="block">
+        <h2>Bottom Sidebar</h2>
+        <p>
+          Lorem ipsum dolor sit amet quam sed fermentum elementum ultrices aliquam erat volutpat. Proin porta ornare libero, eu aliquet tortor vestibulum sed.
+        </p>
+      </div>
+      <!--./side3-->
+    </aside>
+  </div>
+  <!--./main-->
+  <div class="clr"></div>
+  <footer>
+    <p>Copyright &copy; 2013 - Resproject</p>
+  </footer>
+</div>
+<!--./container-->
+</body>
+</html>
