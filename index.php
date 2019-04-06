ca6912226f98929dafe77f20fbbae75f9f9e2000
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-06 23:56:19 +0200

    bug fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
     if (have_posts()) : 
-        while (have_posts()) : the post(); ?>
+        while (have_posts()) : the_post(); ?>
             <h2><?php the_title()?></h2>
             <?php the_content() ?>
 <?php endwhile;
