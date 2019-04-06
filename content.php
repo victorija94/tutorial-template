3727d894c4c92d9b5b7b12e4bf1e73725b38e16c
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-07 00:48:43 +0200

    bug fix

---
+++ content.php
@@ -0,0 +1,14 @@
+      <!--./art1-->
+      <article class="art1">
+        <header class="art-header">
+          <h2 class="title"><a href="#">Second Post</a></h2>
+          <span>Created by: Brad Traversy on October 3, 2013</span>
+        </header>
+        <!--./art-header-->
+        <p>In in rutrum sapien. Ut tellus nibh, facilisis faucibus pulvinar vitae, sagittis at orci. In sem tellus, faucibus non eros sit amet, tincidunt dictum ligula. Fusce ornare sodales nisi, non aliquet lacus rutrum in. Vestibulum in sem urna. Cras
+          commodo massa enim, non hendrerit lacus pretium vel. </p>
+        <p>Aliquam tristique augue vitae urna aliquet, et volutpat metus gravida. Mauris nec ornare nisl. Sed egestas mi et viverra sollicitudin. Fusce hendrerit adipiscing ipsum, ut suscipit nibh convallis fringilla. Mauris tempus id velit at lacinia. Aenean
+          scelerisque ac sem in accumsan. </p>
+        <a class="readmore" href="#">Read more</a>
+      </article>
+      <!--./art1-->
--- index.php
+++ index.php
@@ -2,32 +2,15 @@
 
   <div id="main">
     <div id="content">
-      <article class="art1">
-        <header class="art-header">
-          <h2 class="title"><a href="#">First Post</a></h2>
-          <span>Created by: Brad Traversy on October 3, 2013</span>
-        </header>
-        <!--./art-header-->
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus arcu est, lacinia et nisi nec, vehicula pretium magna. Nulla ut nisl vel lorem posuere aliquam a feugiat sapien. Suspendisse tincidunt mauris at consequat ornare. Donec condimentum
-          consequat felis a sodales. Donec vestibulum imperdiet nisl id vulputate. </p>
-        <p>Aenean in tellus ut nibh dignissim adipiscing nec eget libero. Donec eu diam quam. Sed fermentum elementum ultrices. Aliquam erat volutpat. Integer sollicitudin lacus quis est elementum tincidunt. Praesent mattis, felis a hendrerit tempus, risus
-          orci mollis massa, a porta lorem libero id purus. </p>
-        <a class="readmore" href="#">Read more</a>
-      </article>
-      <!--./art1-->
-      <article class="art1">
-        <header class="art-header">
-          <h2 class="title"><a href="#">Second Post</a></h2>
-          <span>Created by: Brad Traversy on October 3, 2013</span>
-        </header>
-        <!--./art-header-->
-        <p>In in rutrum sapien. Ut tellus nibh, facilisis faucibus pulvinar vitae, sagittis at orci. In sem tellus, faucibus non eros sit amet, tincidunt dictum ligula. Fusce ornare sodales nisi, non aliquet lacus rutrum in. Vestibulum in sem urna. Cras
-          commodo massa enim, non hendrerit lacus pretium vel. </p>
-        <p>Aliquam tristique augue vitae urna aliquet, et volutpat metus gravida. Mauris nec ornare nisl. Sed egestas mi et viverra sollicitudin. Fusce hendrerit adipiscing ipsum, ut suscipit nibh convallis fringilla. Mauris tempus id velit at lacinia. Aenean
-          scelerisque ac sem in accumsan. </p>
-        <a class="readmore" href="#">Read more</a>
-      </article>
-      <!--./art1-->
+    <?php if (have_posts()) :
+    while (have_posts()) : the_post(); 
+    get_template_part('content', get_post_format());
+        
+     endwhile;
+else:
+    echo "";
+endif; ?>
+
     </div>
     <!--./content-->
     <aside id="sidebar">
