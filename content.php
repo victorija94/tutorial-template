b090ee6534c1a98504124c7460ba5d395de24da5
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-07 00:52:08 +0200

    bug fix

--- content.php
+++ content.php
@@ -1,14 +1,11 @@
       <!--./art1-->
       <article class="art1">
         <header class="art-header">
-          <h2 class="title"><a href="#">Second Post</a></h2>
+          <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <span>Created by: Brad Traversy on October 3, 2013</span>
         </header>
         <!--./art-header-->
-        <p>In in rutrum sapien. Ut tellus nibh, facilisis faucibus pulvinar vitae, sagittis at orci. In sem tellus, faucibus non eros sit amet, tincidunt dictum ligula. Fusce ornare sodales nisi, non aliquet lacus rutrum in. Vestibulum in sem urna. Cras
-          commodo massa enim, non hendrerit lacus pretium vel. </p>
-        <p>Aliquam tristique augue vitae urna aliquet, et volutpat metus gravida. Mauris nec ornare nisl. Sed egestas mi et viverra sollicitudin. Fusce hendrerit adipiscing ipsum, ut suscipit nibh convallis fringilla. Mauris tempus id velit at lacinia. Aenean
-          scelerisque ac sem in accumsan. </p>
-        <a class="readmore" href="#">Read more</a>
+       <?php the_content(); ?>
+        <a class="readmore" href="<?php the_permalink(); ?>">Read more</a>
       </article>
       <!--./art1-->
