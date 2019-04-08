58910de7f79d569f41d7209c3a565759b1cc0458
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-09 00:52:47 +0200

    bug fix

--- functions.php
+++ functions.php
@@ -1,4 +1,14 @@
 <?php
+
+function learningWordPress_setup() {
+    //Navigation Menus
+register_nav_menus(array(
+    'primary' => __('Primary Menu'),
+    'footer' => __('Footer Menu '),
+));
+}
+add_action('after_setup_theme' , 'learningWordPress_setup');
+
 function res_blog_resources(){
     wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');    
 }
