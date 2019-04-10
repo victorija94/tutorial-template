3aa34f808995ed836cfc8f9f52df033060fb87a9
victorija94 <viktorija.cvetanovska@hotmail.com>
2019-04-11 01:25:41 +0200

    changes index

--- index.php
+++ index.php
@@ -24,13 +24,6 @@
 		<?php get_header(); ?>
 
 
-
-  <!-- side-content --> 
- <div class="site-content clearfix">
-
-   <!-- main-column --> 
-   <div class="main-column">
-
    <?php if (have_posts()) :
     while (have_posts()) : the_post(); 
     get_template_part('content', get_post_format());
